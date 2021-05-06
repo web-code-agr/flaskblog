1e15e5dfd0f76c7bec5e2f83aa7cbc40168187d6
iamvaibhav <vaibhavagrawal.iitian@gmail.com>
2021-05-07 00:42:36 +0530

    Completed all the features, next thing is to setup email delivery for password recovery

--- app/routes.py
+++ app/routes.py
@@ -3,30 +3,16 @@ import secrets
 from PIL import Image
 from app import app,db,bcrypt
 from app.models import User,Post
-from flask import render_template, flash, redirect, url_for,request
-from app.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flask import render_template, flash, redirect, url_for,request,abort
+from app.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flask_login import login_user,current_user,logout_user, login_required
 
-
-posts = [
-    {
-        'author':'Vaibhav Kant Agrawal',
-        'title' : 'Post 1',
-        'content' : 'first post content',
-        'date_posted' : 'April 20,2016' 
-    },
-    {
-        'author':'Ayush Agrawal',
-        'title' : 'Post 2',
-        'content' : 'Second post content',
-        'date_posted' : 'August 20,2016' 
-    }
-]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    
+    page = request.args.get('page', 1, int)
+    posts  = Post.query.order_by(Post.date_posted.desc()).paginate(page = page ,per_page = 5)    
     return render_template('home.html', posts = posts)
 
 
@@ -99,3 +85,60 @@ def account():
         form.email.data = current_user.email
     image_file = url_for('static', filename = 'profile_pics/'+ current_user.image_file)
     return render_template('account.html', title = 'Account', image_file= image_file, form = form)
+
+@app.route('/post/new', methods = ['POST', 'GET'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title = form.title.data, content = form.content.data, author = current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('your post have been created successfully', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title = 'New Post', form = form, legend = 'Create New Post')
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title = post.title, post = post)
+
+@app.route('/post/<int:post_id>/update', methods = ['POST','GET'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated', 'success')
+        return redirect(url_for('post', post_id = post.id))
+    elif request.method=="GET":
+        form.title.data = post.title
+        form.content.data = post.content    
+    return render_template('create_post.html', title = 'Update Post', form = form, legend = 'Update Post')
+
+@app.route('/post/<int:post_id>/delete', methods = ['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted', 'success')
+    return redirect(url_for('home'))
+
+
+
+@app.route('/user/<string:username>')
+def user_posts(username):
+    page = request.args.get('page', 1, int)
+    user = User.query.filter_by(username = username).first_or_404()
+    posts  = Post.query.filter_by(author=user).\
+            order_by(Post.date_posted.desc()).\
+            paginate(page = page ,per_page = 5)    
+    return render_template('user_posts.html', posts = posts,user= user)
